8767dab2429f413c2148caa4f7338d70fb264ce8
artcevvv <artcevvv@yandex.kz>
2024-04-26 21:21:05 +0500

    1234

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import { userRouter } from './routes/users.js'
+import {PORT, MONGO_USER, URI} from '../globals.js'
 
 const app= express()
-const PORT = process.env.PORT || 4000
-const uri = `mongodb+srv://artcevvv:MERNRecipes@cluster0.yri1xlx.mongodb.net/recipes?retryWrites=true&w=majority&appName=Cluster0`
+const PORT = PORT
+const uri = URI
 
 app.use(express.json())
 app.use(cors())
