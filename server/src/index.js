0a0321c2c2828f185d0f7655bee4c454b5d4b079
artcevvv <artcevvv@yandex.kz>
2024-04-27 10:11:42 +0500

    frontend

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import { userRouter } from './routes/users.js'
-import {PORT, MONGO_USER, URI} from '../globals.js'
+import {PORT, URI} from '../globals.js'
 
 const app= express()
-const PORT = PORT
 const uri = URI
 
 app.use(express.json())
