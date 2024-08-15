3fc01d9f9c30ca470397bbba8ad65385c287e5ec
artcevvv <artcevvv@yandex.kz>
2024-08-15 12:22:02 +0500

    123

--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,16 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import { userRouter } from './routes/users.js'
 import {PORT, URI} from '../globals.js'
+import { recipesRouter } from './routes/recipes.js'
 
 const app= express()
 const uri = URI
 
 app.use(express.json())
 app.use(cors())
-app.use('/auth', userRouter)
 
+app.use('/auth', userRouter)
+app.use('/recipes', recipesRouter)
 
 mongoose
     .connect(uri)
---
+++ server/src/models/Recipes.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const RecipeSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    instructions: {
+        type: String,
+        required: true,
+    },
+    ingredients:[{
+        type: String,
+        required: true,
+    }],
+    imageURL: {
+        type: String,
+        required: false,
+    },
+    cookingTime: {
+        type: Number,
+        required: true
+    },
+    userOwner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    }
+})
+
+export const RecipeModel = mongoose.model('recipes', RecipeSchema)
